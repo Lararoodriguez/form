1e21300acc900347399a0f17b4e49197b794cec7
Lararoodriguez <82426688+Lararoodriguez@users.noreply.github.com>
2024-05-22 21:07:47 -0300

    Add files via upload
    Termino del curso, re diseño y caso de uso

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function mostrarAlerta() {
-    alert("¡Hola bienvenid@! Ingresa con tu email y contraseña");
+    alert("¡Hola bienvenid@! Logueate con tu email y contraseña");
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     mostrarAlerta();
+    
+    // Agregar evento click al botón "Registrarse"
+    var registroBtn = document.getElementById("registroBtn");
+    registroBtn.addEventListener("click", function(event) {
+        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+        if (validateForm()) {
+            alert("¡Registro exitoso!");
+        }
+    });
 });
 
 function validateForm() {
@@ -13,23 +22,23 @@ function validateForm() {
     var codigoAreaMovil = form.codigoAreaMovil.value;
     var numeroMovil = form.numeroMovil.value;
 
-    if (!/^([0-9]{2})$/.test(codigoAreaFijo)) {
-        alert("Por favor, ingrese un código de área válido para teléfono fijo (formato: 123).");
+    if (!/^([0-9]{3})$/.test(codigoAreaFijo)) {
+        alert("Por favor, ingrese un código de área válido para teléfono fijo (formato: 011).");
         return false;
     }
 
-    if (!/^([0-9]{4}-[0-9]{4})$/.test(numeroFijo)) {
-        alert("Por favor, ingrese un número de teléfono fijo válido (formato: 4567-8901).");
+    if (!/^([0-9]{8})$/.test(numeroFijo)) {
+        alert("Por favor, ingrese un número de teléfono fijo válido (formato: 45678901).");
         return false;
     }
 
-    if (!/^([0-9]{2})$/.test(codigoAreaMovil)) {
-        alert("Por favor, ingrese un código de área válido para teléfono móvil (formato: 123).");
+    if (!/^([0-9]{3})$/.test(codigoAreaMovil)) {
+        alert("Por favor, ingrese un código de área válido para teléfono móvil (formato: 011).");
         return false;
     }
 
-    if (!/^([0-9]{4}-[0-9]{4})$/.test(numeroMovil)) {
-        alert("Por favor, ingrese un número de teléfono móvil válido (formato: 4567-8901).");
+    if (!/^([0-9]{8})$/.test(numeroMovil)) {
+        alert("Por favor, ingrese un número de teléfono móvil válido (formato: 45678901).");
         return false;
     }
 
